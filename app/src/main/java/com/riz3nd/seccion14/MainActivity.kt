c40c492777380ffc9d8eb99f50acf41c565df03c
Riz3nd <rizend1996@gmail.com>
2022-10-22 13:52:12 -0500

    IMPLEMENTACIÓN  -  Redimensión de casillas
    - Se crea un método para redimensionar las casillas respecto al tamaño de la pantalla.
    - Se crean los métodos para asignar aleatoriamente la posición de inicio del caballo en el tablero.
    - Se crea un método para reiniciar el tablero.

--- app/src/main/java/com/riz3nd/seccion14/MainActivity.kt
+++ app/src/main/java/com/riz3nd/seccion14/MainActivity.kt
@@ -1,16 +1,105 @@
 package com.riz3nd.seccion14
 
+import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.View
-import android.view.Window
-import android.view.WindowManager
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TableRow
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    var cellSelected_x = 0
+    var cellSelected_y = 0
+    private lateinit var board:Array<IntArray>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initScreenGame()
+        resetBoard()
+        setFirstPosition()
+    }
 
+    /**
+     * Metodo para reiniciar el tablero
+     * @author Oscar Argaez
+     */
+    private fun resetBoard() {
+        // 0 Casilla libre
+        // 1 Casilla marcada
+        // 2 Bonus
+        // 9 Opcion del movimiento actual
+
+        board = arrayOf(
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0),
+        )
+    }
+
+    private fun initScreenGame() {
+        setSizeBoard()
+        hideMessage()
+    }
+
+    private fun setSizeBoard() {
+        var iv:ImageView
+        val display =  windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        val width = size.x
+
+        var width_dp = (width / resources.displayMetrics.density)
+
+        var lateralMarginsDP = 0
+        val width_cell = (width_dp - lateralMarginsDP)/8
+        val height_cell = width_cell
+
+        for(i in 0..7){
+            for (j in 0..7){
+                iv = findViewById(resources.getIdentifier("c$i$j","id", packageName))
+                var height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, height_cell, resources.displayMetrics).toInt()
+                var width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, width_cell, resources.displayMetrics).toInt()
+                iv.layoutParams = TableRow.LayoutParams(width, height)
+            }
+        }
+    }
+
+    private fun hideMessage() {
+        var lyMessage = findViewById<LinearLayout>(R.id.lyMessage)
+        lyMessage.visibility = View.GONE
+    }
+
+    private fun setFirstPosition(){
+        var x = 0
+        var y = 0
+        x = (0..7).random()
+        y = (0..7).random()
+        cellSelected_x = x
+        cellSelected_y = x
+        seletCell(x, y)
+    }
+
+    private fun seletCell(x: Int, y: Int) {
+        paintHorseCell(cellSelected_x, cellSelected_y, "previus_cell")
+        cellSelected_x = x
+        cellSelected_y = y
+        paintHorseCell(x, y, "selected_cell")
+    }
+
+    private fun paintHorseCell(x: Int, y: Int, color: String) {
+        var iv:ImageView = findViewById(resources.getIdentifier("c$x$y","id", packageName))
+        iv.setBackgroundColor(ContextCompat.getColor(this, resources.getIdentifier(color, "color", packageName)))
+        iv.setImageResource(R.drawable.horse)
     }
 
     fun checkCellClicked(view: View){
