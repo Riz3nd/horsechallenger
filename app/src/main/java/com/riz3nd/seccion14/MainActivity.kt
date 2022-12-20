fc68b55e14d5e98ae260d134d625ce2e4db0bb24
Oscar Argaez <oscarargaes@wposs.com>
2022-12-20 17:07:45 -0500

    196. Gestion de control de movimiento.

--- app/src/main/java/com/riz3nd/seccion14/MainActivity.kt
+++ app/src/main/java/com/riz3nd/seccion14/MainActivity.kt
@@ -5,19 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.View
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TableRow
-import android.widget.TextView
+import android.widget.*
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
-    var cellSelected_x = 0
-    var cellSelected_y = 0
+    private var cellSelected_x = 0
+    private var cellSelected_y = 0
     private lateinit var board:Array<IntArray>
     private var options = 0
-    var nameColorBlack = "black_cell"
-    var nameColorWhite = "white_cell"
+    private var nameColorBlack = "black_cell"
+    private var nameColorWhite = "white_cell"
+    private var levelMoves = 64
+    private var moves = 64
+    private var movesRequired = 4
+    private var bonus = 0
+    private var width_Bonus = 0
+    private var checkMovement = true
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,6 +69,8 @@ class MainActivity : AppCompatActivity() {
         val width_cell = (width_dp - lateralMarginsDP)/8
         val height_cell = width_cell
 
+        width_Bonus = 2 * width_cell.toInt()
+
         for(i in 0..7){
             for (j in 0..7){
                 iv = findViewById(resources.getIdentifier("c$i$j","id", packageName))
@@ -91,13 +97,93 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun seletCell(x: Int, y: Int) {
+        moves--
+        var tvModesData = findViewById<TextView>(R.id.tvMovesData)
+        tvModesData.text = moves.toString()
+
+        growProgressBonus()
+        if(board[x][y] == 2){
+            bonus++
+            var tvBonusData = findViewById<TextView>(R.id.tvBonusData)
+            var tvSumBonus = findViewById<TextView>(R.id.tvSumBonus)
+            tvSumBonus.visibility = View.VISIBLE
+            tvBonusData.visibility = View.VISIBLE
+            tvBonusData.text = "$bonus"
+        }
+
         board[x][y] = 1
         paintHorseCell(cellSelected_x, cellSelected_y, "previus_cell")
         cellSelected_x = x
         cellSelected_y = y
         clearOptions()
         paintHorseCell(x, y, "selected_cell")
+        checkMovement = true
         checkOptions(x, y)
+
+        if(moves > 0){
+            checkMoveBonus()
+            checkGameOver(x, y)
+        }
+        else showMessage("You Win!", "Next Level", false)
+    }
+
+    private fun checkGameOver(x: Int, y: Int) {
+        if(options == 0){
+            if (bonus == 0){
+                showMessage("Game Over", "Try Again!", true)
+            } else {
+                checkMovement = false
+            }
+        }
+    }
+
+    fun showMessage(title:String, message:String, status:Boolean){
+        var lyMessage = findViewById<LinearLayout>(R.id.lyMessage)
+        var tvIntroLevel = findViewById<TextView>(R.id.tvTitleMessage)
+        var tvScoreMessage = findViewById<TextView>(R.id.tvScoreMessage)
+        var btnNextLevel = findViewById<Button>(R.id.btnNextLevel)
+        var tvTimeData = findViewById<TextView>(R.id.tvTimeData)
+
+        lyMessage.visibility = View.VISIBLE
+        if(status){
+            tvScoreMessage.text = "Score: ${levelMoves - moves}/$levelMoves"
+        }else{
+            tvScoreMessage.text = "${tvTimeData.text}"
+        }
+        tvIntroLevel.text = title
+        btnNextLevel.text = message
+    }
+
+    private fun  growProgressBonus() {
+        var moves_done = levelMoves - moves
+        var bonus_done = moves_done / movesRequired
+        var moves_rest = movesRequired * (bonus_done)
+        var bonus_grow = moves_done - moves_rest
+        var v = findViewById<View>(R.id.vNewBonus)
+        var widthBonus = ((width_Bonus/movesRequired) * bonus_grow).toFloat()
+        var height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8f, resources.displayMetrics).toInt()
+        var width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, widthBonus, resources.displayMetrics).toInt()
+        v.setLayoutParams(TableRow.LayoutParams(width, height))
+    }
+
+    private fun checkMoveBonus() {
+        if(moves % movesRequired == 0){
+            var bonusCell_x = 0
+            var bonusCell_y = 0
+            var bonusCell = false
+            while (bonusCell == false){
+                bonusCell_x = (0..7).random()
+                bonusCell_y = (0..7).random()
+                if(board[bonusCell_x][bonusCell_y] == 0) bonusCell = true
+            }
+            board[bonusCell_x][bonusCell_y] = 2
+            paintBonusCell(bonusCell_x, bonusCell_y)
+        }
+    }
+
+    private fun paintBonusCell(x: Int, y: Int) {
+        var iv: ImageView = findViewById(resources.getIdentifier("c$x$y", "id", packageName))
+        iv.setImageResource(R.drawable.bonus)
     }
 
     private fun clearOptions() {
@@ -132,7 +218,7 @@ class MainActivity : AppCompatActivity() {
         checkMove(x, y, -1, -2)   // check move left - bottom long
         checkMove(x, y, -2, -1)   // check move left long bottom
         var tvOptionsData = findViewById<TextView>(R.id.tvOptonsData)
-        tvOptionsData.text = options.toString()
+        tvOptionsData.text = "$options"
     }
 
     private fun checkMove(x: Int, y: Int, mov_x: Int, mov_y: Int) {
@@ -143,7 +229,7 @@ class MainActivity : AppCompatActivity() {
                 board[option_x][option_y] == 2){
                 options++
                 paintOptions(option_x, option_y)
-                board[option_x][option_y] = 9
+                if(board[option_x][option_y] == 0) board[option_x][option_y] = 9
 
             }
         }
@@ -179,17 +265,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkCell(x: Int, y: Int) {
-        var dif_x = x - cellSelected_x
-        var dif_y = y - cellSelected_y
-        var checkTrue = false
-        if (dif_x == 1 && dif_y == 2)   checkTrue = true // right - top long
-        if (dif_x == 1 && dif_y == -2)  checkTrue = true // right - bottom long
-        if (dif_x == 2 && dif_y == 1)   checkTrue = true // right long - top
-        if (dif_x == 2 && dif_y == -1)  checkTrue = true // right long - bottom
-        if (dif_x == -1 && dif_y == 2)  checkTrue = true // left - top long
-        if (dif_x == -1 && dif_y == -2) checkTrue = true // left - bottom long
-        if (dif_x == -2 && dif_y == 1)  checkTrue = true // left long - top
-        if (dif_x == -2 && dif_y == -1) checkTrue = true // left long - bottom
+        var checkTrue = true
+        if (checkMovement){
+            var dif_x = x - cellSelected_x
+            var dif_y = y - cellSelected_y
+            checkTrue = false
+            if (dif_x == 1 && dif_y == 2)   checkTrue = true // right - top long
+            if (dif_x == 1 && dif_y == -2)  checkTrue = true // right - bottom long
+            if (dif_x == 2 && dif_y == 1)   checkTrue = true // right long - top
+            if (dif_x == 2 && dif_y == -1)  checkTrue = true // right long - bottom
+            if (dif_x == -1 && dif_y == 2)  checkTrue = true // left - top long
+            if (dif_x == -1 && dif_y == -2) checkTrue = true // left - bottom long
+            if (dif_x == -2 && dif_y == 1)  checkTrue = true // left long - top
+            if (dif_x == -2 && dif_y == -1) checkTrue = true // left long - bottom
+        }else{
+            if(board[x][y] != 1){
+                bonus--
+                var tvBonusData = findViewById<TextView>(R.id.tvBonusData)
+                tvBonusData.text = "$bonus"
+            }
+        }
         if(board[x][y] == 1) checkTrue = false
         if(checkTrue) seletCell(x, y)
     }
